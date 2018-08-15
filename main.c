5f9fdd391125b1e0707f411d432558ac640c5b3b
ash <anneso.hartmann@web.de>
2018-08-15 13:35:53 +0200

    implemented blind minesweeper

--- main.c
+++ main.c
@@ -22,14 +22,15 @@ int button_index(int x_coordinate, int y_coordinate, int num_of_buttons, int b_s
 	return index;
 }
 
-void reveal(int button_index, int num_of_buttons){
+void reveal(int button_index, int num_of_buttons, int array[]){
 	//reveals number/mine of field button_index
 
 	//if this doesn't work check how booleans are treated during recursion
 
-	//might be necessary to mark revealed fields with 10
 
-	int number;// = look up number in array
+	int number = array[button_index];//look up number in array
+	array[button_index] = 10;
+	//printf("number %d\n", number);
 	//booleans noting which neighbors the field has
 	bool no_upper = true;
 	bool no_lower = true;
@@ -39,6 +40,8 @@ void reveal(int button_index, int num_of_buttons){
 	switch(number){
 		case -1:
 			//BOOM
+			printf("BOOM!\n you died.\n");
+
 			break;
 		case 1:
 		case 2:
@@ -49,46 +52,48 @@ void reveal(int button_index, int num_of_buttons){
 		case 7:
 		case 8:
 			//reveal number
+			printf("revealed %d\n", number);
 			break;
 		case 0:
 			//reveal empty field
+			printf("revealed empty\n");
 			//set array to '10' or something else marking field as revealed
 			//recursively call reveal on neighbors
 			if ((button_index% num_of_buttons) != 0){
 				//field has left neighbor
-				reveal(button_index - 1, button_index);
+				reveal(button_index - 1, button_index, array);
 				no_left = false;
 			} 
 			if ((button_index%num_of_buttons) != (num_of_buttons - 1)){
 				//field has right neighbor
-				reveal(button_index + 1, num_of_buttons);
+				reveal(button_index + 1, num_of_buttons, array);
 				no_right = false;
 			}
 			if (button_index >= num_of_buttons){
 				//field has upper neighbor
-				reveal(button_index - num_of_buttons, num_of_buttons);
+				reveal(button_index - num_of_buttons, num_of_buttons, array);
 				no_upper = false;
 			}
 			if (button_index < (num_of_buttons * (num_of_buttons - 1))){
 				//field has lower neighbor
-				reveal(button_index + num_of_buttons, num_of_buttons);
+				reveal(button_index + num_of_buttons, num_of_buttons, array);
 				no_lower = false;
 			}
 			if (!no_left && !no_upper){
 				//field has upper left neighbor
-				reveal(button_index - (num_of_buttons + 1), num_of_buttons);
+				reveal(button_index - (num_of_buttons + 1), num_of_buttons, array);
 			}
 			if (!no_upper && !no_right){
 				//field has upper right neighbor
-				reveal(button_index - num_of_buttons + 1, num_of_buttons);
+				reveal(button_index - num_of_buttons + 1, num_of_buttons, array);
 			}
 			if (!no_left && !no_lower){
 				//field has lower left neighbor
-				reveal(button_index + (num_of_buttons - 1), num_of_buttons);
+				reveal(button_index + (num_of_buttons - 1), num_of_buttons, array);
 			}
 			if (!no_lower && !no_right){
 				//field has lower right neighbor
-				reveal(button_index + num_of_buttons + 1, num_of_buttons);
+				reveal(button_index + num_of_buttons + 1, num_of_buttons, array);
 			}
 			break;
 		case 10: //necessary to avoid creating infinite loop of neighboring empty fields calling reveal on each other 
@@ -107,10 +112,12 @@ int main(void){
 	SDL_Event e;
 	bool quit = false;
 	int button_size = 50;//size of a button in pixels
-	int num_buttons = 15; //number of buttons in one row/column
+	int num_buttons = 5; //number of buttons in one row/column
 	int index; //index of a button
 	int x;
 	int y;
+	int dummy[25] = {-1,1,0,1,1,1,1,1,2,-1,1,1,2,-1,2,1,-1,2,1,1,1,1,1,0,0};
+	
 
 	SDL_Window *window; //create a window
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -138,12 +145,12 @@ int main(void){
 				x = e.button.x; //get coordinates of mouse click
 				y = e.button.y;
 				index = button_index(x, y, num_buttons, button_size); //get index
-				//testing stuff				
-				printf("%d\n", index);
+						
 
 				if (e.button.button == SDL_BUTTON_LEFT){ //left
-					printf("clicked left at: %d, %d \n", x, y);
-					//reveal(index, num_buttons)
+					//printf("clicked left at: %d, %d \n", x, y);
+					printf("clicked button %d\n", index);
+					reveal(index, num_buttons, dummy);
 					
 				}
 				if (e.button.button == SDL_BUTTON_RIGHT){//right
